3859cf17b44e2cd12ce312604380af803fc7a3cc
Noura <culurienneldoreth@gmail.com>
2021-10-04 13:27:18 -0400

    added installation instructions

--- README.md
+++ README.md
@@ -2,8 +2,86 @@
 
 This project studio explores Emotion AI with large interactive inflatable sculptures. This repo is for collecting code snippets used for this project.
 
-# Acknowledgements
+# Setup for Raspberry Pi Zero W
 
+## Install OpenCV 
+
+```console
+sudo pip3 install --upgrade opencv-python
+
+sudo apt-get install libatlas-base-dev
+
+sudo apt-get install libhdf5-dev
+
+sudo apt-get install libjasper-dev
+
+sudo apt-get install libqtgui4
+
+sudo pip3 install --upgrade numpy
+
+```
+
+## Other installs
+
+This is what I did to get it working on my raspberry pi zero w.
+
+```console
+sudo apt-get install python3-numpy
+
+sudo apt-get install libblas-dev
+
+sudo apt-get install liblapack-dev
+
+sudo apt-get install libatlas-base-dev
+
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+
+sudo apt-get -y install libxvidcore-dev libx264-dev
+
+sudo apt-get -y install qt4-dev-tools libatlas-base-dev
+
+sudo apt-get install python3-h5py
+
+sudo apt-get install cmake
+
+sudo apt-get install libboost-all-dev
+
+```
+
+Also installed Python libraries
+
+```console
+sudo pip3 install cmake
+
+sudo pip3 install dlib
+
+sudo pip3 install face_recognition
+
+sudo pip3 install keras
+
+sudo pip3 install tflite
+
+```
+
+Download the wheel for the custom tensor flow lite build for raspberry pi zero w and install it. 
+
+```console
+
+cd
+
+wget https://github.com/prettyflyforabeeguy/tf_lite_on_pi_zero/blob/master/whl/armv6l/tflite_runtime-2.3.1-cp37-cp37m-linux_armv6l.whl?raw=true
+
+mv 'tflite_runtime-2.3.1-cp37-cp37m-linux_armv6l.whl?raw=true' tflite_runtime-2.3.1-cp37-cp37m-linux_armv6l.whl
+
+sudo pip3 install tflite_runtime-2.3.1-cp37-cp37m-linux_armv6l.whl
+
+```
+
+
+
+# Acknowledgements
 
 [Priyanka Dwivedi - Pretrained Emotion Model](https://github.com/priya-dwivedi/face_and_emotion_detection)
 
---
+++ emotion_detection.py
@@ -0,0 +1,81 @@
+from PIL import Image
+import numpy as np
+import face_recognition
+import cv2
+import tflite_runtime.interpreter as tflite
+
+image_filename = "rageface.jpeg"
+
+model_filename = "model.tflite"
+
+
+
+# DETECTING AND CROPPING FACES
+# image = face_recognition.load_image_file("../image2.jpg")
+image = cv2.imread(image_filename)
+
+# getting the bounding boxes of all faces detected
+# each element of this list is a tuple (top, right, bottom, left)
+face_locations = face_recognition.face_locations(image)
+print('face_locations:  ', face_locations)
+
+# getting the bounding box of the first face detected
+# this assumes a face was detected
+top, right, bottom, left = face_locations[0]
+
+# cropping the image to the face bounding box
+image_edited = image[top:bottom, left:right]
+
+# resizing the image
+image_edited = cv2.resize(image_edited, (48,48))
+
+# make the image black and white
+image_edited = cv2.cvtColor(image_edited, cv2.COLOR_BGR2GRAY)
+
+# write the image to a file
+cv2.imwrite('image_edited.jpg', image_edited)
+
+emotion_dict = { \
+                '0': 'Angry', \
+                '1': 'Disgust', \
+                '2': 'Fear', \
+                '3': 'Happy', \
+                '4': 'Neutral', \
+                '5': 'Sad', \
+                '6': 'Surprise'}
+
+emotion_list = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+
+interpreter = tflite.Interpreter(model_path=model_filename)
+print('interpreter:  ', interpreter)
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+print('input details:  ', input_details)
+
+output_details = interpreter.get_output_details()
+print('output details:  ', output_details)
+
+input_shape = input_details[0]['shape']
+print('input shape:  ', input_shape)
+
+image_edited = np.reshape(image_edited, input_shape)
+
+image_edited = image_edited.astype('float32')
+
+interpreter.set_tensor(input_details[0]['index'], image_edited )
+
+interpreter.invoke()
+
+output_data = interpreter.get_tensor(output_details[0]['index'])
+
+print('output data ', output_data)
+
+# processing the output data to get the emotion name
+
+for i in range(7):
+    print('Emotion ', emotion_list[i], ':  ', output_data[0][i])
+
+
+
+
+
